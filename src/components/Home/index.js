2cdfb2e9ccad5610b7fcd9fe25214ef7c0392710
Flippin Bits <flippinbits@majikbytes.com>
2024-03-01 11:46:42 -0800

    Implement file uploading

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [file, setFile] = useState(null);
+
   return (
     <div className="Home">
       <h1>Upload a video:</h1>
-      <input type="file" />
+      <input type="file" onChange={e => setFile(e.target.files[0])} />
+      {file &&
+       <button onClick={() => {
+         const formData = new FormData();
+         formData.append('file', file);
+
+         fetch(
+           `${process.env.API_URI}/v1/videos`,
+           {
+             method: 'POST',
+             body: formData
+           }
+         )
+       }}>Upload</button>
+      }
     </div>
   );
 };
