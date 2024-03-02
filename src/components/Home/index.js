6771bdb94d831f561927f6c0dadca3f46fcce5a0
Flippin Bits <flippinbits@majikbytes.com>
2024-03-02 07:27:48 -0800

    Add additional file data when uploading file

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,24 +2,32 @@ import { useState } from 'react';
 
 const Home = () => {
   const [file, setFile] = useState(null);
+  const [metadata, setMetadata] = useState({});
 
   return (
     <div className="Home">
       <h1>Upload a video:</h1>
       <input type="file" onChange={e => setFile(e.target.files[0])} />
       {file &&
-       <button onClick={() => {
-         const formData = new FormData();
-         formData.append('file', file);
+       <div>
+         <input onChange={e => setMetadata({...metadata, [e.target.name]: e.target.value}) } className="input" type="text" name="title" placeholder="title" />
+         <input onChange={e => setMetadata({...metadata, [e.target.name]: e.target.value}) } className="input" type="text" name="description" placeholder="description" />
+         <input onChange={e => setMetadata({...metadata, [e.target.name]: e.target.value}) } className="input" type="text" name="keywords" placeholder="keywords" />
+         <button onClick={() => {
+           const formData = new FormData();
+           formData.append('file', file);
+           formData.append('metadata', JSON.stringify(metadata));
 
-         fetch(
-           `${process.env.API_URI}/v1/videos`,
-           {
-             method: 'POST',
-             body: formData
-           }
-         )
-       }}>Upload</button>
+           fetch(
+             `${process.env.API_URI}/v1/videos`,
+             {
+               method: 'POST',
+               credentials: 'include',
+               body: formData
+             }
+           )
+         }}>Upload</button>
+       </div>
       }
     </div>
   );
