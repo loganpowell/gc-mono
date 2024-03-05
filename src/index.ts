3d7ba37f2ff377050456b7cdb99c9b5e3a6d83d4
Flippin Bits <flippinbits@majikbytes.com>
2024-03-04 22:32:18 -0800

    Add lint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,125 +1,139 @@
-import { Hono } from 'hono';
-import { cors } from 'hono/cors';
-import { parse } from 'cookie';
-import { drizzle } from 'drizzle-orm/d1';
-import { eq, and } from 'drizzle-orm';
-import * as jose from 'jose';
+import { parse } from "cookie";
+import { drizzle } from "drizzle-orm/d1";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import * as jose from "jose";
 
-import * as schema from '../db/schema';
-import { User, UserIdentity, AuthProvider, Video } from './models';
+import * as schema from "../db/schema";
+import { User, UserIdentity, AuthProvider, Video } from "./models";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('*', cors({
-  origin: ['http://localhost:8788', 'https://medic.gaza-care.com', 'http://localhost:8789', 'https://admin.gaza-care.com'],
-  allowHeaders: ['Content-Type', 'Authorization'],
-  allowMethods: ['POST', 'GET', 'OPTIONS'],
-  exposeHeaders: ['Content-Length'],
-  maxAge: 600,
-  credentials: true,
-}));
+app.use(
+  "*",
+  cors({
+    origin: [
+      "http://localhost:8788",
+      "https://medic.gaza-care.com",
+      "http://localhost:8789",
+      "https://admin.gaza-care.com",
+    ],
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
 
-app.options('*', (c) => {
-  return new Response(null, {status: 204});
-})
+app.options("*", (c) => {
+  return new Response(null, { status: 204 });
+});
 
-app.use('*', async (context, next) => {
+app.use("*", async (context, next) => {
   const db = drizzle(context.env.DB, { schema });
-  context.set('db', db);
+  context.set("db", db);
   await next();
 });
 
-const authenticate = async c => {
+const authenticate = async (c) => {
   return await currentUser(c);
 };
 
-app.post('/v1/auth/google/success', async c => {
+app.post("/v1/auth/google/success", async (c) => {
   const { creds, userType } = await c.req.raw.json();
   const credentials = jose.decodeJwt(creds);
 
-  let authProvider = await AuthProvider(c.var.db).create({name: 'google'});
-  authProvider ||= await AuthProvider(c.var.db).withName({name: 'google'});
+  let authProvider = await AuthProvider(c.var.db).create({ name: "google" });
+  authProvider ||= await AuthProvider(c.var.db).withName({ name: "google" });
   let user = await currentUser(c);
 
   if (!user) {
     const uid = crypto.randomUUID();
     const username = `goog-${credentials.sub}`;
-    user = await User(c.var.db).create({username, uid, userType});
+    user = await User(c.var.db).create({ username, uid, userType });
     if (user) {
-      const identity = await UserIdentity(c.var.db).create({userID: user.id, authProviderID: authProvider.id, providerUserID: credentials.sub, profile: JSON.stringify(credentials)});
+      const identity = await UserIdentity(c.var.db).create({
+        userID: user.id,
+        authProviderID: authProvider.id,
+        providerUserID: credentials.sub,
+        profile: JSON.stringify(credentials),
+      });
     }
     if (!user) {
       user = await User(c.var.db).withUsername(username);
     }
   }
 
-  return new Response(
-    JSON.stringify({...user, ...credentials}),
-    {
-      headers: {'Set-Cookie': `gcre_session=${user.uid};Path=/;SameSite=Strict;Secure;HttpOnly`}
-    }
-  );
-});
-
-app.get('/v1/session', async c => {
-  const user = await currentUser(c);
-
-  if (user) {
-    return new Response(JSON.stringify(user), {status: 200});
-  }
-
-  return new Response('unauthorized', {status: 401});
-});
-
-app.post('/v1/videos', async c => {
-  const user = await authenticate(c);
-
-  const formData = await c.req.raw.formData();
-  const file = formData.get('file');
-  const metadata = JSON.parse(await formData.get('metadata'));
-
-  const fileData = await file.arrayBuffer();
-  const digest = await crypto.subtle.digest('MD5', fileData);
-  const bytes = Array.from(new Uint8Array(digest));
-  const md5 = bytes.map(b => b.toString(16).padStart(2, '0')).join('');
-
-  await c.env.R2.put(file.name, fileData);
-
-  const video = await Video(c.var.db).create({
-    uploaderID: user.id,
-    title: metadata.title,
-    description: metadata.description,
-    keywords: metadata.keywords,
-    filename: file.name,
-    filetype: file.type,
-    filesize: file.size,
-    md5Hash: md5,
-    metadata: JSON.stringify(metadata),
-  }) || await Video(c.var.db).getByMD5Hash(md5);
-
-  return new Response(JSON.stringify({...video}), {status: 200});
-});
-
-app.get('/v1/logout', async c => {
-  return new Response(null, {
-    status: 204,
+  return new Response(JSON.stringify({ ...user, ...credentials }), {
     headers: {
-      'Clear-Site-Data': "\"cookies\""
-    }
+      "Set-Cookie": `gcre_session=${user.uid};Path=/;SameSite=Strict;Secure;HttpOnly`,
+    },
   });
 });
 
-const currentUID = async context => {
-  return parse(context.req.raw.headers.get('cookie') || '')['gcre_session'];
-}
+app.get("/v1/session", async (c) => {
+  const user = await currentUser(c);
 
-const currentUser = async context => {
+  if (user) {
+    return new Response(JSON.stringify(user), { status: 200 });
+  }
+
+  return new Response("unauthorized", { status: 401 });
+});
+
+app.post("/v1/videos", async (c) => {
+  const user = await authenticate(c);
+
+  const formData = await c.req.raw.formData();
+  const file = formData.get("file");
+  const metadata = JSON.parse(await formData.get("metadata"));
+
+  const fileData = await file.arrayBuffer();
+  const digest = await crypto.subtle.digest("MD5", fileData);
+  const bytes = Array.from(new Uint8Array(digest));
+  const md5 = bytes.map((b) => b.toString(16).padStart(2, "0")).join("");
+
+  await c.env.R2.put(file.name, fileData);
+
+  const video =
+    (await Video(c.var.db).create({
+      uploaderID: user.id,
+      title: metadata.title,
+      description: metadata.description,
+      keywords: metadata.keywords,
+      filename: file.name,
+      filetype: file.type,
+      filesize: file.size,
+      md5Hash: md5,
+      metadata: JSON.stringify(metadata),
+    })) || (await Video(c.var.db).getByMD5Hash(md5));
+
+  return new Response(JSON.stringify({ ...video }), { status: 200 });
+});
+
+app.get("/v1/logout", async (c) => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Clear-Site-Data": '"cookies"',
+    },
+  });
+});
+
+const currentUID = async (context) => {
+  return parse(context.req.raw.headers.get("cookie") || "").gcre_session;
+};
+
+const currentUser = async (context) => {
   const uid = await currentUID(context);
 
   if (uid) {
     const user = await User(context.var.db).withUID(uid);
 
-    if (!user) return null;
+    if (!user) {
+      return null;
+    }
 
     return user;
   }
--- src/models/index.js
+++ src/models/index.js
@@ -1,68 +1,83 @@
-import { eq, and } from 'drizzle-orm';
+import { eq, and } from "drizzle-orm";
 
-import { users, authProviders, userIdentities, videos } from '../../db/schema';
+import { users, authProviders, userIdentities, videos } from "../../db/schema";
 
-export const User = db => ({
-  withUID: async uid => (await db
-    .select()
-    .from(users)
-    .where(eq(users.uid, uid))
-    .limit(1)
-  )[0],
-  withUsername: async username => (await db
-    .select()
-    .from(users)
-    .where(eq(users.username, username))
-    .limit(1)
-  )[0],
-  create: async ({
-    username, uid, userType
-  }) => (await db
-    .insert(users)
-    .values({username, uid, userType})
-    .onConflictDoNothing()
-    .returning()
-  )[0]
+export const User = (db) => ({
+  withUID: async (uid) =>
+    (await db.select().from(users).where(eq(users.uid, uid)).limit(1))[0],
+  withUsername: async (username) =>
+    (
+      await db.select().from(users).where(eq(users.username, username)).limit(1)
+    )[0],
+  create: async ({ username, uid, userType }) =>
+    (
+      await db
+        .insert(users)
+        .values({ username, uid, userType })
+        .onConflictDoNothing()
+        .returning()
+    )[0],
 });
 
-export const AuthProvider = db => ({
-  create: async ({
-    name
-  }) => (await db
-    .insert(authProviders)
-    .values({name})
-    .onConflictDoNothing()
-    .returning()
-  )[0],
-  withName: async ({
-    name
-  }) => (await db
-    .select()
-    .from(authProviders)
-    .where(eq(authProviders.name, name))
-    .limit(1)
-  )[0]
+export const AuthProvider = (db) => ({
+  create: async ({ name }) =>
+    (
+      await db
+        .insert(authProviders)
+        .values({ name })
+        .onConflictDoNothing()
+        .returning()
+    )[0],
+  withName: async ({ name }) =>
+    (
+      await db
+        .select()
+        .from(authProviders)
+        .where(eq(authProviders.name, name))
+        .limit(1)
+    )[0],
 });
 
-export const UserIdentity = db => ({
-  create: async ({
-    userID, authProviderID, providerUserID, profile
-  }) => (await db
-    .insert(userIdentities)
-    .values({userID, authProviderID, providerUserID, profile})
-    .onConflictDoNothing()
-    .returning()
-  )[0]
+export const UserIdentity = (db) => ({
+  create: async ({ userID, authProviderID, providerUserID, profile }) =>
+    (
+      await db
+        .insert(userIdentities)
+        .values({ userID, authProviderID, providerUserID, profile })
+        .onConflictDoNothing()
+        .returning()
+    )[0],
 });
 
-export const Video = db => ({
+export const Video = (db) => ({
   create: async ({
-    uploaderID, title, description, keywords, filename, filetype, filesize, md5Hash, metadata 
-  }) => (await db
-    .insert(videos)
-    .values({uploaderID, title, description, keywords, filename, filetype, filesize, md5Hash, metadata})
-    .onConflictDoNothing()
-    .returning()
-  )[0],
-  getByMD5Hash: async md5 => (await db.select().from(videos).where(eq(videos.md5Hash, md5)))
+    uploaderID,
+    title,
+    description,
+    keywords,
+    filename,
+    filetype,
+    filesize,
+    md5Hash,
+    metadata,
+  }) =>
+    (
+      await db
+        .insert(videos)
+        .values({
+          uploaderID,
+          title,
+          description,
+          keywords,
+          filename,
+          filetype,
+          filesize,
+          md5Hash,
+          metadata,
+        })
+        .onConflictDoNothing()
+        .returning()
+    )[0],
+  getByMD5Hash: async (md5) =>
+    await db.select().from(videos).where(eq(videos.md5Hash, md5)),
 });
