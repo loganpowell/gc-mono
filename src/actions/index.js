b45ac7c913a19be783086cddc0d021d7d2961080
Logan Powell <logan@tepper.cmu.edu>
2024-03-13 20:44:22 -0400

    adds ability to approve a video

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,36 +1,51 @@
-const BASE_URI = process.env.API_URI;
+const BASE_URI = process.env.API_URI
 
 const authenticate = (dispatch, navigate, redirectUrl) => {
-  dispatch({ type: "REQUESTING_AUTH" });
+    dispatch({ type: 'REQUESTING_AUTH' })
 
-  fetch(`${BASE_URI}/v1/session`, {
-    method: 'GET',
-    credentials: 'include',
-  }).then(async response => {
-    if (response.ok) {
-      const json = await response.json();
-      return {ok: true, ...json};
-    }
+    fetch(`${BASE_URI}/v1/session`, {
+        method: 'GET',
+        credentials: 'include',
+    })
+        .then(async (response) => {
+            if (response.ok) {
+                const json = await response.json()
+                return { ok: true, ...json }
+            }
 
-    return {ok: false, status: response.status};
-  })
-    .then(json => {
-      if (!json.ok) return navigate(`/login?redirect_to=${redirectUrl}`);
+            return { ok: false, status: response.status }
+        })
+        .then((json) => {
+            if (!json.ok) return navigate(`/login?redirect_to=${redirectUrl}`)
 
-      dispatch({type: 'RECEIVED_AUTH', data: {...json}});
-    });
-};
+            dispatch({ type: 'RECEIVED_AUTH', data: { ...json } })
+        })
+}
 
 const logout = (dispatch, navigate) => {
-  dispatch({type: 'REQUESTING_LOGOUT'});
+    dispatch({ type: 'REQUESTING_LOGOUT' })
 
-  fetch(`${BASE_URI}/v1/logout`, {
-    method: 'GET',
-    credentials: 'include',
-  }).then(response => {
-    dispatch({type: 'RECEIVED_LOGOUT'});
-    navigate('/login');
-  });
-};
+    fetch(`${BASE_URI}/v1/logout`, {
+        method: 'GET',
+        credentials: 'include',
+    }).then((response) => {
+        dispatch({ type: 'RECEIVED_LOGOUT' })
+        navigate('/login')
+    })
+}
 
-export { authenticate, logout };
+const getVideos = (dispatch) => {
+    dispatch({ type: 'REQUESTING_TODOS' })
+
+    return fetch(`${BASE_URI}/v1/todos`, {
+        method: 'GET',
+        credentials: 'include',
+    })
+        .then(async (response) => await response.json())
+        .then((json) => {
+            dispatch({ type: 'RECEIVED_TODOS', data: json })
+            return json
+        })
+}
+
+export { authenticate, logout, getVideos }
