e4ecd77ce800939832ddf02168022da6a3dd31a0
Logan Powell <logan@tepper.cmu.edu>
2024-04-03 23:58:38 -0400

    updates eslint settings

--- apps/api/src/test.js
+++ apps/api/src/test.js
@@ -1,3 +1,5 @@
+import { describe, it } from "global";
+
 describe("API", () => {
   it("runs", () => {
     console.log("MUST ADD TESTS!");
--- packages/config-eslint/index.js
+++ packages/config-eslint/index.js
@@ -1,45 +1,50 @@
-const { resolve } = require('node:path')
+const { resolve } = require("node:path");
 
-const project = resolve(process.cwd(), 'tsconfig.json')
+const project = resolve(process.cwd(), "tsconfig.json");
 
-export default {
-    env: {
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  globals: {
+    JSX: true,
+  },
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js", "**/*.css"],
+
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+      node: {
+        extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "prefer-const": "warn",
+  },
+  overrides: [
+    {
+      files: ["*.config.js"],
+      env: {
         node: true,
+      },
     },
-    parser: '@typescript-eslint/parser',
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-    plugins: ['@typescript-eslint'],
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2020,
-    },
-    globals: {
-        JSX: true,
-    },
-    ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js', '**/*.css'],
-
-    settings: {
-        'import/resolver': {
-            typescript: {
-                project,
-            },
-            node: {
-                extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'],
-            },
-        },
-    },
-    rules: {
-        '@typescript-eslint/no-non-null-assertion': 'warn',
-        '@typescript-eslint/no-unused-vars': 'warn',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/ban-ts-comment': 'off',
-    },
-    overrides: [
-        {
-            files: ['*.config.js'],
-            env: {
-                node: true,
-            },
-        },
-    ],
-}
+  ],
+};
